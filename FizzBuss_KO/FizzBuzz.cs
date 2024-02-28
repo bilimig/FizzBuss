201a9b85aef1d7f7c2af140c679b4f1012f0ed78
stud <stud@W04N-P317D08>
2024-02-28 17:40:55 +0100

    Add project files.

---
+++ FizzBuss_KO/FizzBuzz.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuss_KO
+{
+    internal class FizzBuzz
+    {
+        private int _x;
+        public FizzBuzz() 
+        {
+
+        }
+
+        public void FizzBuzzing()
+        {
+            Console.WriteLine("Podaj liczbe:");
+
+            string input = Console.ReadLine();
+            
+            Console.Clear();
+            
+            int.TryParse(input, out _x);
+            
+            for(int i = 1; i <= _x; i++)
+            {
+                if(mod3(i) && mod5(i))
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+            
+                else if(mod3(i))
+                {
+                    Console.WriteLine("Fizz");
+                }
+                
+                else if (mod5(i)) {
+                    Console.WriteLine("Buzz");
+                }
+                
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+        private bool mod3(int num)  
+        {
+            if (num % 3 == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        private bool mod5(int num)
+        {
+            if (num % 5 == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    
+    }
+}
---
+++ FizzBuss_KO/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace FizzBuss_KO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Hello, World!");
+
+            FizzBuzz fizzBuzz = new FizzBuzz();
+            fizzBuzz.FizzBuzzing();
+
+        }
+    }
+}
